1296f053e92f98216c9dab9dfe89d96952802393
José Alejandro Berrío Marín <joseberrio@ebsolutionscorp.com>
2018-03-14 00:55:35 -0500

    ejemplo con componentes y subcomponentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,29 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { VotesComponent } from './votes/votes.component';
+import { TodoFormComponent } from './todo-form/todo-form.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoService } from './todo/todo.service';
+import { AsyncFakeasyncComponent } from './async-fakeasync/async-fakeasync.component';
+import { AsyncFakeasyncService } from './async-fakeasync/async-fakeasync.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    VotesComponent,
+    TodoFormComponent,
+    TodoComponent,
+    AsyncFakeasyncComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
   ],
   providers: [
+    TodoService, 
+    AsyncFakeasyncService
   ],
   bootstrap: [AppComponent]
 })
