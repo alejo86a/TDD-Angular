673f71540460c5121c2d084927c8ab909e7d0bbd
José Alejandro Berrío Marín <joseberrio@ebsolutionscorp.com>
2018-03-15 02:37:52 -0500

    solucion error test

--- src/app/canditate/candidate.component.spec.ts
+++ src/app/canditate/candidate.component.spec.ts
@@ -1,14 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CandidateComponent } from './candidate.component';
+import { VotesComponent } from '../votes/votes.component';
 
-fdescribe('CandidateComponent', () => {
+describe('CandidateComponent', () => {
   let component: CandidateComponent;
   let fixture: ComponentFixture<CandidateComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CandidateComponent ]
+      declarations: [ CandidateComponent, VotesComponent]
     })
     .compileComponents();
   }));
