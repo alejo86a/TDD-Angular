a890818d62a7bdf48908c7c1e94303e2d9831961
José Alejandro Berrío Marín <joseberrio@ebsolutionscorp.com>
2018-03-15 02:31:02 -0500

    crear componente candidato

---
+++ src/app/canditate/candidate.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-candidate',
+  templateUrl: './candidate.component.html',
+  styleUrls: ['./candidate.component.css']
+})
+export class CandidateComponent {
+
+ 
+}
